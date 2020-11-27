1d93889d82d1935363a5a74a727a2c9bd0b1f81c
fazalfarhan01 <fazal.farhan@gmail.com>
2020-11-27 14:56:32 +0530

    verified contact form working

--- assets/js/logic/contactForm.js
+++ assets/js/logic/contactForm.js
@@ -8,13 +8,15 @@ $(document).ready(function () {
         }, 2000);
     });
 });
-function showSubmitAlert() {
-    alert("Your Request has been submitted successfully.\nWe will contact you shortly.\n\nThank you..!");
-    setTimeout(function () {
-        console.log("Clearing Form");
-        clearContactForm();
-    }, 2000);
-}
+
+// Similat Workaround
+// function showSubmitAlert() {
+//     alert("Your Request has been submitted successfully.\nWe will contact you shortly.\n\nThank you..!");
+//     setTimeout(function () {
+//         console.log("Clearing Form");
+//         clearContactForm();
+//     }, 2000);
+// }
 
 function clearContactForm() {
     document.getElementById("contactForm").reset();
