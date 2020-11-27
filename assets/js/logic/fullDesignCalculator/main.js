aff9c8acb3ea751492a98c8eda26d7c9dd098e2b
fazalfarhan01 <fazal.farhan@gmail.com>
2020-11-28 00:29:15 +0530

    displaying the step 1 works

---
+++ assets/js/logic/fullDesignCalculator/main.js
@@ -0,0 +1,114 @@
+function startCalculations() {
+    // Grab Form Values
+    var variablesFromForm = Array.from(document.querySelectorAll("#filterDesignerForm input")).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
+
+    // Show Results
+    document.getElementById("filterCalculations").style.display = "block";
+
+    // Convert to Rad/Sec
+    if (document.getElementById("frequencyType").value == "hz") {
+        variablesFromForm.passBandFrequency = parseFloat((variablesFromForm.passBandFrequency * Math.PI * 2).toFixed(2));
+        variablesFromForm.stopBandFrequency = parseFloat((variablesFromForm.stopBandFrequency * Math.PI * 2).toFixed(2));;
+    }
+
+    // Display The Values
+    document.getElementById("display_minPassBandGain").innerText = `\\(Minimum Pass Band Gain (Ap): ${variablesFromForm.minPassBandGain}\\)`;
+    document.getElementById("display_maxStopBandGain").innerText = `\\(Maximum Stop Band Gain (As): ${variablesFromForm.maxStopBandGain}\\)`;
+    document.getElementById("display_passBandFrequency").innerText = `\\(Pass Band Frequency (rad/s): ${variablesFromForm.passBandFrequency}\\)`;
+    document.getElementById("display_stopBandFrequency").innerText = `\\(Stop Band Frequency (rad/s): ${variablesFromForm.stopBandFrequency}\\)`;
+
+    // Show Values in the Formula
+    var normalFormula = `\\(N\\geq\\frac{\\log_{10}[\\frac{\\frac{1}{${variablesFromForm.maxStopBandGain}^{2}}-1}{\\frac{1}{${variablesFromForm.minPassBandGain}^{2}}-1}]}{2\\times\\log_{10}\\frac{${variablesFromForm.stopBandFrequency}}{${variablesFromForm.passBandFrequency}}}\\)`;
+    var decibleFormula = `\\(N \\geq \\frac{\\log_{10} [ \\frac{10^{0.1 \\times ${variablesFromForm.maxStopBandGain}}-1}{10^{0.1 \\times ${variablesFromForm.minPassBandGain}}-1}]}{2 \\times \\log_{10} \\frac{${variablesFromForm.stopBandFrequency}}{${variablesFromForm.passBandFrequency}}}\\)`;
+
+    // Display Formulas Based On Gain Type
+    if (document.getElementById("gainType").value == "decibles") {
+        document.getElementById("orderFormulaDecible").style.display = "block";
+        document.getElementById("orderFormulaNormal").style.display = "none";
+        document.getElementById("orderFormulaWithValues").innerText = decibleFormula;
+    } else {
+        document.getElementById("orderFormulaNormal").style.display = "block";
+        document.getElementById("orderFormulaDecible").style.display = "none";
+        document.getElementById("orderFormulaWithValues").innerText = normalFormula;
+    }
+    MathJax.typeset();
+
+}
+
+function getCutoff() {
+    var variablesFromForm = Array.from(document.querySelectorAll("#filterDesignerForm input")).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
+    // console.log(variablesFromForm);
+
+    if (document.getElementById("frequencyType").value == "hz") {
+        variablesFromForm.passBandFrequency = variablesFromForm.passBandFrequency * Math.PI * 2;
+        variablesFromForm.stopBandFrequency = variablesFromForm.stopBandFrequency * Math.PI * 2;
+    }
+
+    console.log(variablesFromForm);
+    console.log(getOrder());
+    if (document.getElementById("gainType").value == "decibles") {
+        console.log("Triggered decibels")
+        var frequency1 = cutofffreqfordecibles(variablesFromForm.minPassBandGain, variablesFromForm.passBandFrequency, getOrder());
+        var frequency2 = cutofffreqfordecibles(variablesFromForm.maxStopBandGain, variablesFromForm.stopBandFrequency, getOrder());
+    } else {
+        console.log("Triggered Normal")
+        var frequency1 = cutofffreqfornodecibles(variablesFromForm.minPassBandGain, variablesFromForm.passBandFrequency, getOrder());
+        var frequency2 = cutofffreqfornodecibles(variablesFromForm.maxStopBandGain, variablesFromForm.stopBandFrequency, getOrder());
+    }
+    cutofffrequency = (frequency1 + frequency2) / 2;
+    return cutofffrequency;
+}
+
+
+function getOrder() {
+    var variablesFromForm = Array.from(document.querySelectorAll("#filterDesignerForm input")).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
+    if (document.getElementById("frequencyType").value == "hz") {
+        variablesFromForm.passBandFrequency = variablesFromForm.passBandFrequency * Math.PI * 2;
+        variablesFromForm.stopBandFrequency = variablesFromForm.stopBandFrequency * Math.PI * 2;
+    }
+
+    if (document.getElementById("gainType").value == "decibles") {
+        var order = orderFromDecibles(variablesFromForm.minPassBandGain, variablesFromForm.maxStopBandGain, variablesFromForm.passBandFrequency, variablesFromForm.stopBandFrequency);
+    } else {
+        var order = orderFromNonDecibles(variablesFromForm.minPassBandGain, variablesFromForm.maxStopBandGain, variablesFromForm.passBandFrequency, variablesFromForm.stopBandFrequency);
+    }
+    return order;
+}
+
+
+function cutofffreqfornodecibles(Gain, freq, _filteOrderN) {
+    numerator = freq;
+    denominator = Math.pow((1 / Math.pow(Gain, 2) - 1), 1 / (2 * _filteOrderN));
+    return (numerator / denominator);
+}
+
+function cutofffreqfordecibles(Gain, freq, _filteOrderN) {
+    numerator = freq;
+    denominator = Math.pow((Math.pow(10, 0.1 * Gain) - 1), 1 / (2 * _filteOrderN));
+    return (numerator / denominator);
+}
+
+function orderFromNonDecibles(minPassBandGain, maxStopBandGain, passBandFreq, stopBandFreq) {
+    var numerator = Math.log10((1 / (Math.pow(maxStopBandGain, 2)) - 1) / (1 / (Math.pow(minPassBandGain, 2)) - 1));
+    var denominator = 2 * Math.log10(stopBandFreq / passBandFreq);
+    return Math.ceil(numerator / denominator);
+}
+
+function orderFromDecibles(minPassBandGain, maxStopBandGain, passBandFreq, stopBandFreq) {
+    var numerator = Math.log10((Math.pow(10, 0.1 * maxStopBandGain) - 1) / (Math.pow(10, 0.1 * minPassBandGain) - 1));
+    var denominator = 2 * Math.log10(stopBandFreq / passBandFreq);
+    return Math.ceil(numerator / denominator);
+}
+
+function showResult(newLine) {
+    resultContainer = document.getElementById("filterResults");
+    resultContainer.style.display = "block";
+    newLine = newLine + "\n";
+    resultContainer.innerText = resultContainer.innerText + newLine;
+}
+
+function clearResults() {
+    resultContainer = document.getElementById("filterResults");
+    resultContainer.innerText = "";
+    resultContainer.style.display = "none";
+}
---
+++ assets/js/logic/fullDesignCalculator/mathjaxOptions.js
@@ -0,0 +1,17 @@
+MathJax = {
+    tex: {
+        inlineMath: [
+            ['$', '$'],
+            ['\\(', '\\)']
+        ]
+    },
+    chtml: {
+        scale: 1.7,
+    },
+    svg: {
+        scale: 2,
+    },
+    options: {
+        enableMenu: false,
+    }
+};
