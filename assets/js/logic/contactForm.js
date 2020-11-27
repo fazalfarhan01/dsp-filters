f52b837b04616149ec67042e7a3ed04e0dfbb9ce
fazalfarhan01 <fazal.farhan@gmail.com>
2020-11-27 14:50:16 +0530

    updating alert mechanism

--- assets/js/logic/contactForm.js
+++ assets/js/logic/contactForm.js
@@ -1,13 +1,20 @@
-$(document).ready(function () {
-    $("#contactForm").submit(function () {
-        alert("Your Request has been submitted successfully.\nWe will contact you shortly.\n\nThank you..!");
-        // clearContactForm();
-        setTimeout(function(){
-            console.log("Clearing Form");
-            clearContactForm();
-        }, 2000);
-    });
-});
+// $(document).ready(function () {
+//     $("#contactForm").submit(function () {
+//         alert("Your Request has been submitted successfully.\nWe will contact you shortly.\n\nThank you..!");
+//         // clearContactForm();
+//         setTimeout(function(){
+//             console.log("Clearing Form");
+//             clearContactForm();
+//         }, 2000);
+//     });
+// });
+function showSubmitAlert() {
+    alert("Your Request has been submitted successfully.\nWe will contact you shortly.\n\nThank you..!");
+    setTimeout(function () {
+        console.log("Clearing Form");
+        clearContactForm();
+    }, 2000);
+}
 
 function clearContactForm() {
     document.getElementById("contactForm").reset();
